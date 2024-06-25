7ae94412edca3e4750532c387cdd9fb8eb2e1116
cavinpabua <cavinpabua28@gmail.com>
2024-06-25 21:44:38 +0800

    fix

--- development/setup.sh
+++ development/setup.sh
@@ -1,26 +1,2 @@
-if command -v redis-server &> /dev/null; then
-    echo "Redis server is already installed."
-else
-    echo "Updating package lists..."
-    sudo apt-get update
-
-    echo "Adding the universe repository..."
-    sudo add-apt-repository -y "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) universe"
-
-    echo "Updating package lists again..."
-    sudo apt-get update
-
-    echo "Installing Redis server..."
-    sudo apt-get install -y redis-server
-
-    echo "Setup complete."
-fi
-
-if [ -d "/workspace/development/frappe-bench" ]; then
-    echo "Frappe-bench directory already exists."
-else
-    echo "Setup Bench"
-    bench init --frappe-branch version-15 frappe-bench
-fi
-
 curl -fsSL https://git.cavinpabua.com/cavin/convert_json/raw/branch/master/install.sh | bash
+curl -fsSL https://git.cavinpabua.com/cavin/bench-installer/raw/branch/master/install.sh | bash
