a815265caf8842409c22bf13a454f4132840b645
Cavin <166345236+cavinwela@users.noreply.github.com>
2024-06-28 14:58:19 +0800

    Update setup.sh

--- development/setup.sh
+++ development/setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 
-curl -fsSL https://git.cavinpabua.com/cavin/convert_json/raw/branch/master/install.sh | bash
 curl -fsSL https://git.cavinpabua.com/cavin/bench-installer/raw/branch/master/install.sh | bash
